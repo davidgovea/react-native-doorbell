ddb130a66bc1850252585720872967894141ba8e
David Govea <govea.d@gmail.com>
2018-08-26 22:02:33 -0700

    provide rootViewController to doorbell

--- ios/RNDoorbell.m
+++ ios/RNDoorbell.m
@@ -12,8 +12,9 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(show:(NSString *)appId apiKey:(NSString *)apiKey)
 {
+    UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
     Doorbell *feedback = [Doorbell doorbellWithApiKey:apiKey appId:appId];
-    [feedback showFeedbackDialogInViewController:self completion:^(NSError *error, BOOL isCancelled) {
+    [feedback showFeedbackDialogInViewController:rootViewController completion:^(NSError *error, BOOL isCancelled) {
         if (error) {
             NSLog(@"%@", error.localizedDescription);
         }
