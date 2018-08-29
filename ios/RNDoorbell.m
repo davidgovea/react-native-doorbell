7d30420499a80efb4286207b2851db786bee04ea
David Govea <govea.d@gmail.com>
2018-08-28 20:46:33 -0700

    take a stab at the ios side

--- ios/RNDoorbell.m
+++ ios/RNDoorbell.m
@@ -10,10 +10,28 @@
 }
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(show:(NSString *)appId apiKey:(NSString *)apiKey)
+RCT_EXPORT_METHOD(show:(NSString *)appId apiKey:(NSString *)apiKey options:(NSDictionary *)options)
 {
     UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
     Doorbell *feedback = [Doorbell doorbellWithApiKey:apiKey appId:appId];
+
+    for (id key in options) {
+      if ([key isEqualToString:@"email"]) {
+        feedback.email = [options objectForKey:key]
+      } else if ([key isEqualToString:@"name"]) {
+        feedback.name = [options objectForKey:key]
+      } else if ([key isEqualToString:@"hideEmailField"]) {
+        feedback.showEmail = false; // TODO - actually check option val
+      } else if ([key isEqualToString:@"hidePoweredBy"]) {
+        feedback.showPoweredBy = false; // TODO - actually check option val
+      } else if ([key isEqualToString:@"hidePoweredBy"]) {
+        NSDictionary *properties = [options objectForKey:key];
+        for (id propertyName in properties) {
+          [feedback addPropertyWithName:propertyName AndValue:[properties objectForKey:propertyName]];
+        }
+      }
+    }
+
     [feedback showFeedbackDialogInViewController:rootViewController completion:^(NSError *error, BOOL isCancelled) {
         if (error) {
             NSLog(@"%@", error.localizedDescription);
@@ -22,4 +40,3 @@ RCT_EXPORT_METHOD(show:(NSString *)appId apiKey:(NSString *)apiKey)
 }
 
 @end
-  
